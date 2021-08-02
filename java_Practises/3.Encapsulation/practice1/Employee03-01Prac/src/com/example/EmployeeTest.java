db580cbe089b6026131e723b5eb2d3ac25acdd2a
james-strom <james.stromnes@gmail.com>
2021-08-02 14:43:06 +0200

    Practice 3 complete and updated

--- java_Practises/3.Encapsulation/practice1/Employee03-01Prac/src/com/example/EmployeeTest.java
+++ java_Practises/3.Encapsulation/practice1/Employee03-01Prac/src/com/example/EmployeeTest.java
@@ -11,10 +11,13 @@ public class EmployeeTest {
         Engineer eng = new Engineer(101, "Jane Smith", "012-34-5678", 120_345.77);
         Manager mgr = new Manager(207, "Barbara Johnson", "054-12-2367", 109_501.36, "US Marketing");
         Admin adm = new Admin(304, "Bill Munroe", "108-23-6509", 75_002.34);
-        Director dir = new Director (12, "Susan Wheeler", "099-45-2340", 120_567.46, "Global Marketing", 1_000_000.00);
+        Director dir = new Director(12, "Susan Wheeler", "099-45-2340", 120_567.46, "Global Marketing", 1_000_000.00);
         eng.printEmployee();
         adm.printEmployee();
         mgr.printEmployee();
         dir.printEmployee();
+        mgr.setName("Barbara Johnson-Smythe");
+        mgr.raiseSalary(10_000.00);
+        mgr.printEmployee();
     }
 }
