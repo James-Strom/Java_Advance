2cfb36510e84c9b87eb85069837facf99007cc55
james-strom <james.stromnes@gmail.com>
2021-08-03 11:15:55 +0200

    Practice 4.1 complete

---
+++ java_Practises/4.Polymorphism/practice1/Employee04-01Prac/src/com/example/business/EmployeeStockPlan.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.business;
+
+import com.example.domain.Director;
+import com.example.domain.Employee;
+import com.example.domain.Manager;
+
+/**
+ *
+ * @author james
+ */
+public class EmployeeStockPlan {
+
+    private final int employeeShares = 10;
+    private final int managerShares = 100;
+    private final int directorShares = 1000;
+
+    public int grantStock(Employee emp) {
+        // Stock is granted based on the employee type
+        if (emp instanceof Director) {
+            return directorShares;
+        } else {
+            if (emp instanceof Manager) {
+                return managerShares;
+            } else {
+                return employeeShares;
+            }
+        }
+    }
+}
