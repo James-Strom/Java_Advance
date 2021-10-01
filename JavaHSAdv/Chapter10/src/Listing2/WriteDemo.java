1343a70ed4cfb36d57f70254d47bf87c8e505d6b
james-strom <james.stromnes@gmail.com>
2021-10-01 13:00:07 +0200

    HS chapter 10 update

---
+++ JavaHSAdv/Chapter10/src/Listing2/WriteDemo.java
@@ -0,0 +1,12 @@
+package Listing2;
+
+// Demonstrate System.out.write().
+class WriteDemo {
+    public static void main(String args[]) {
+        int b;
+
+        b = 'X';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
