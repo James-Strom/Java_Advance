5a82e53b7a86da59c8513a91f9f0f9523ac9134d
james-strom <james.stromnes@gmail.com>
2021-10-26 11:01:42 +0200

    Git soccer app update 2

--- SoccerProject/SoccerApp/src/com/example/mypackage/Soccer.java
+++ SoccerProject/SoccerApp/src/com/example/mypackage/Soccer.java
@@ -5,31 +5,42 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 
-public class Soccer {
+        public class Soccer {
 
-    public static void main(String[] args) {
-        // pass the path to the file as a parameter
-        File file = new File("C:\\Computer Files\\Code College\\Java_advanced\\Classwork\\SoccerProject\\test.txt");
-        Scanner sc = null;
-        String match,team1, team2, score1, score2, left, right;
-        String [] sides;
-        try {
-            sc = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            public static void main(String[] args) {
+                // pass the path to the file as a parameter
+                File file = new File("C:\\Computer Files\\Code College\\Java_advanced\\Classwork\\SoccerProject\\test.txt");
+                Scanner sc = null;
+                String match, team1, team2, score1, score2;
+                int leagueTotal;
+
+                String [] sides, l1,l2;
+                try {
+                    sc = new Scanner(file);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                while (sc.hasNextLine()) {
+                    match = sc.nextLine();
+                    sides = match.split(",");
+                    l1 = sides[0].split(" ");
+                    team1 = l1[0];
+                    score1 = l1[1];
+
+                    l2 = sides[1].split(" ");
+                    team2 = l2[0];
+                    score2 = l2[1];
+
+                    int teamScore1 = Integer.parseInt(score1);
+                    int teamScore2 = Integer.parseInt(score2);
+                    // update the scoreboard
+                               if (teamScore1 > teamScore2) {
+                                    leagueTotal+=3
+
+                               }
+                }
+            }
         }
 
-        while (sc.hasNextLine()) {
-            match = sc.nextLine();
-            sides = match.split(",");
-            left = sides[0];
-            right = sides[1];
 
-            System.out.println(left);
-            System.out.println(right);
-
-            team1 = left.split();
-            team2 = left.split()
-        }
-    }
-}
